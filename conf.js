6670e7785aa399c6a0a938d6d7105885c19e0eb4
sunqi <sunqi_925@163.com>
2017-07-12 14:52:59 +0800

    add configure file

---
+++ conf.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    secret:"UQRfGakB41PtEQlGqLwFemT1tYnBmzXJ",
+    database:{
+        host:"127.0.0.1",
+        user:"root",
+        password:"root",
+        database:"td",
+        connectionLimit:5
+    },
+    port:8081,
+    version:"0.1.0",
+    logFilePath:"./app.log",
+    logLevel:"error"
+};
